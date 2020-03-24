48c475e8cb66fbbbee0057723b4f02eb8c2d0f67
Meghan Stang <mstang15@gmail.com>
2020-03-24 07:32:18 -0600

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Song.destroy_all
+Artist.destroy_all
+
+#artists
+carly_rae = Artist.create(name: "Carly Rae")
+billie_eilish = Artist.create(name: "Billie Eilish")
+journey = Artist.create(name: "Journey")
+
+
+#songs
+carly_rae.songs.create(title: "I Really Like You",
+                   length: 200,
+                   play_count: 3000)
+carly_rae.songs.create(title: "Call Me Maybe",
+                   length: 100,
+                   play_count: 1000)
+billie_eilish.songs.create(title: "Bad Guy",
+                   length: 300,
+                   play_count: 2000)
+billie_eilish.songs.create(title: "Bury A Friend",
+                     length: 250,
+                     play_count: 1500)
+journey.songs.create(title: "Don't Stop Believin'",
+                     length: 300,
+                     play_count: 3000000)
